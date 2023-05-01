dcaa117c7d07088a3af1a2d0011a8d0d20796477
xenedium <ahmed@abderraziq.com>
2023-05-01 20:04:04 +0100

    Simple logout btn

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,10 +1,18 @@
+import { Button, Theme, YStack } from "tamagui";
 import { NavigationProps } from "../types";
+import { supabase } from "../lib/SupabaseClient";
+import { useUserTheme } from "../Hooks/useUserTheme";
 
 
 type Props = NavigationProps<'Home'>;
 
-export function Home ({ navigation }: Props) {
+export function Home({ navigation }: Props) {
+    const theme = useUserTheme();
     return (
-        <></>
+        <Theme name={theme}>
+            <YStack f={1} jc="center" ai="center" bg="$background">
+                <Button onPress={() => supabase.auth.signOut()}>Logout</Button>
+            </YStack>
+        </Theme>
     );
 }
